e91fcb7fafd823d96de4d44d9c81db1a04d41955
sixpawnstuido <sixpawnstudios@gmail.com>
2024-08-06 11:20:52 +0300

    aa

---
+++ Assets/BarController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarController : SerializedMonoBehaviour
+{
+    public static BarController Instance;
+    
+    public Dictionary<PlayerType, NewTarget> targets;
+
+    public Image playerImage;
+    public Image opponentImage;
+
+    public int targetAmount;
+
+    [SerializeField] private Transform holder;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ChangeProgress(PlayerType playerType,int hexagonAmount)
+    {
+        holder.DOPunchScale(new Vector3(-.05f, 0.05f, 0), .3f, 5) .SetId(transform.GetHashCode());
+        if (playerType==PlayerType.PLAYER)
+        {
+            float increaseAmount = Mathf.InverseLerp(0, targetAmount, hexagonAmount);
+            var fillAmount= playerImage.fillAmount + increaseAmount;
+            var fillAmountOpponent= opponentImage.fillAmount - increaseAmount;
+
+            DOVirtual.Float(playerImage.fillAmount, fillAmount, .3f, (v) => playerImage.fillAmount = v);
+            DOVirtual.Float(opponentImage.fillAmount, fillAmountOpponent, .3f, (v) => opponentImage.fillAmount = v);
+            
+            if (fillAmount>=1)
+            {
+                LevelManager.Instance.OpenNextLevelPanel();
+            }
+            targets[PlayerType.PLAYER].splashVFX.Stop();
+            targets[PlayerType.PLAYER].splashVFX.Play();
+        }
+        else
+        {
+            float increaseAmount = Mathf.InverseLerp(0, targetAmount, hexagonAmount);
+            
+            var fillAmount= opponentImage.fillAmount + increaseAmount;
+            var fillAmountOpponent= playerImage.fillAmount - increaseAmount;
+            
+            DOVirtual.Float(playerImage.fillAmount, fillAmountOpponent, .2f, (v) => playerImage.fillAmount = v);
+            DOVirtual.Float(opponentImage.fillAmount, fillAmount, .2f, (v) => opponentImage.fillAmount = v);
+            if (fillAmount>=1)
+            {
+                LevelManager.Instance.OpenFailedPanel();
+            }
+            
+            targets[PlayerType.OPPONENT].splashVFX.Stop();
+            targets[PlayerType.OPPONENT].splashVFX.Play();
+        }
+
+        if (playerImage.fillAmount>opponentImage.fillAmount)
+        {
+            playerImage.transform.SetSiblingIndex(0);
+        }
+        else
+        {
+            opponentImage.transform.SetSiblingIndex(0);
+        }
+    }
+    
+}
---
+++ Assets/NewTarget.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewTarget : MonoBehaviour
+{
+    public Image image;
+
+    public Transform targetTransform;
+
+    public ParticleSystem splashVFX;
+
+}
